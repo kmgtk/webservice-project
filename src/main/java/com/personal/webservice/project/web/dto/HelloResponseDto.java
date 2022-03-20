ca9951df622382c61c24e7728e54d7fa6bfeb2a2
kmgtk <56664447+kmgtk@users.noreply.github.com>
2022-03-20 09:57:51 +0900

    1st commit

---
+++ src/main/java/com/personal/webservice/project/web/dto/HelloResponseDto.java
@@ -0,0 +1,16 @@
+package com.personal.webservice.project.web.dto;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Getter
+@RequiredArgsConstructor
+public class HelloResponseDto {
+
+    private final String name;
+
+    private final int amount;
+
+
+}

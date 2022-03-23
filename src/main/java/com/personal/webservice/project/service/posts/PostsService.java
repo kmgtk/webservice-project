4d15e7cd1e960efbd0e6a0051f49516aa76ae7df
kmgtk <56664447+kmgtk@users.noreply.github.com>
2022-03-24 00:19:57 +0900

    3st commit

--- src/main/java/com/personal/webservice/project/service/posts/PostsService.java
+++ src/main/java/com/personal/webservice/project/service/posts/PostsService.java
@@ -2,12 +2,16 @@ package com.personal.webservice.project.service.posts;
 
 import com.personal.webservice.project.domain.posts.Posts;
 import com.personal.webservice.project.domain.posts.PostsRepository;
+import com.personal.webservice.project.web.dto.PostsListResponseDto;
 import com.personal.webservice.project.web.dto.PostsResponseDto;
 import com.personal.webservice.project.web.dto.PostsSaveRequestDto;
 import com.personal.webservice.project.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -32,6 +36,16 @@ public class PostsService {
         Posts entity = postsRepository.findById(id).orElseThrow(()->new IllegalArgumentException("해당 게시글이 없습니다. id="+id));
 
         return new PostsResponseDto(entity);
+    }
+
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc(){
+        return postsRepository.findAllDesc().stream().map(PostsListResponseDto::new).collect(Collectors.toList());
+    }
+
+    public void delete (Long id){
+        Posts posts = postsRepository.findById(id).orElseThrow(()->new IllegalArgumentException("해당 게시글이 없습니다. Id="+id));
+        postsRepository.delete(posts);
     }
 
 }
--- src/main/java/com/personal/webservice/project/web/PostsApiContorller.java
+++ src/main/java/com/personal/webservice/project/web/PostsApiContorller.java
@@ -31,6 +31,12 @@ public class PostsApiContorller {
         return postsService.findById(id);
     }
 
+    @DeleteMapping("/api/v1/posts/{id}")
+    public Long delete(@PathVariable Long id){
+        postsService.delete(id);
+        return id;
+    }
+
 
 
 }
